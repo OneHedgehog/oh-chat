7f4371fda0e027e661de3d3ff2b8684a27cb5704
OneHedgehog <aperteda@gmail.com>
2018-07-23 00:33:49 +0300

    first commit

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent, NavbarComponent, SidebarComponent
+  ],
+  imports: [
+    BrowserModule, BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
