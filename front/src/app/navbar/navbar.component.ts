f3f095068ce31d52764e9e7e4336335f0303d64e
OneHedgehog <aperteda@gmail.com>
2018-07-24 00:53:52 +0300

    search

--- front/src/app/navbar/navbar.component.ts
+++ front/src/app/navbar/navbar.component.ts
@@ -1,9 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrls: ['./navbar.component.sass'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({transform: 'translateX(-100%)'}),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({transform: 'translateX(100%)'}))
+      ])
+    ])
+  ]
 })
 export class NavbarComponent implements OnInit {
 
