b1672e8403b693655c046aca86a2c9c6e0637a0f
OneHedgehog <--replace-all>
2018-08-06 01:03:28 +0300

    messages

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -11,11 +11,12 @@ import { ContactsComponent } from './main-app-chat/contacts/contacts.component';
 import { MainAppChatComponent } from './main-app-chat/main-app-chat.component';
 import { MessengerComponent } from './main-app-chat/messenger/messenger.component';
 import { CalendarComponent } from './main-app-chat/calendar/calendar.component';
+import { MessagePreviewComponent } from './main-app-chat/contacts/message-preview/message-preview.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, NavbarComponent, SidebarComponent, ContactsComponent, MainAppChatComponent, MessengerComponent, CalendarComponent
+    AppComponent, NavbarComponent, SidebarComponent, ContactsComponent, MainAppChatComponent, MessengerComponent, CalendarComponent, MessagePreviewComponent
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule
